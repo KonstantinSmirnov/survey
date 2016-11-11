73fec51d6bd46531f71992130ae4fa4bed7b3e6d
konstantinsmirnov <k.smi@mail.ru>
2016-11-12 02:00:57 +0300

    Add mailer setup

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Survey
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => ENV['SMTP_USER_NAME'],
+      :password             => ENV['SMTP_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
