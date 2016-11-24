7018efbbb6826271287239d8f50a3efbd4a5d9cd
konstantinsmirnov <k.smi@mail.ru>
2016-11-24 23:44:36 +0300

    Set up locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = current_user.locale.to_sym || I18n.default_locale
+    if current_user
+      I18n.locale = current_user.locale.to_sym || I18n.default_locale
+    else
+      I18n.locale = extract_locale_from_accept_language_header
+    end
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
