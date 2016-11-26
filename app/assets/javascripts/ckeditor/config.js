8b4b88d8d698825a6dd1bce0cde294c3e76c0129
konstantinsmirnov <k.smi@mail.ru>
2016-11-27 00:19:56 +0300

    Update wysywyg editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,9 +2,10 @@ CKEDITOR.editorConfig = function( config ) {
 config.toolbar_basic = [
     [ 'Undo', 'Redo' ],
     [ 'Bold', 'Italic', 'Underline'],
+    [ 'TextColor', 'BGColor' ],
     [ 'NumberedList', 'BulletedList' ],
     [ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-    [ 'Link', 'Unlink','-' ]
+    [ 'Link', 'Unlink', 'Blockquote' ]
 ];
 //You may reconfigure toolbar for all sessions
 config.toolbar = config.toolbar_basic;
