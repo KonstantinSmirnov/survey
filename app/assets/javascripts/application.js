4a1e9cb584c221c08aa57175d7150c220c3573af
konstantinsmirnov <k.smi@mail.ru>
2016-11-30 13:21:26 +0000

    Add only sortable for jquery ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui/widgets/sortable
 //= require tether
 //= require bootstrap-sprockets
 //= require chartkick
