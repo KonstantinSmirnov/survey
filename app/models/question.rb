9de06963aec7bff53ca113aa5bc2d46ad8fcb654
konstantinsmirnov <k.smi@mail.ru>
2016-12-02 07:18:11 +0000

    Change image size for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,7 +10,7 @@ class Question < ApplicationRecord
 
   accepts_nested_attributes_for :answer_variants, :allow_destroy => true
 
-  has_attached_file :image, styles: { medium: "500x3750#", thumb: "100x75#" }
+  has_attached_file :image, styles: { medium: "500x375#", thumb: "100x75#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
